0487d17306ba4a1cbe46b2574bc80462f2ca70b4
Jay Joo <jj534@cornell.edu>
2023-01-07 21:55:00 +0900

    remove console log

--- src/lib/glue/pocketbase.ts
+++ src/lib/glue/pocketbase.ts
@@ -6,6 +6,5 @@ export const pb = new PocketBase('https://thrifthub.fly.dev/');
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
-	console.log('auth onchange', auth);
 	currentUser.set(pb.authStore.model);
 });
