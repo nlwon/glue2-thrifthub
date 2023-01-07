cba3acfcdd71dbe5212bc78199bc7a109fa84506
Jay Joo <jj534@cornell.edu>
2023-01-07 21:50:46 +0900

    init backend

--- src/lib/glue/pocketbase.ts
+++ src/lib/glue/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://thrifthub.fly.dev/');
+export const pb = new PocketBase('https://glue2-sentiment.fly.dev/');
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
