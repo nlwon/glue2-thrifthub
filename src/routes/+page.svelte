d5da81e53e8731a90ef69ad093017d6e0348ca17
Jay Joo <jj534@cornell.edu>
2023-01-10 13:39:49 +0900

    add meilisearch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,41 +7,42 @@
 	import debounce from 'just-debounce-it';
 	import MeiliSearch from 'meilisearch';
 
-	// const client = new MeiliSearch({
-	// 	host: 'https://meilisearch-production-023b.up.railway.app/',
-	// 	apiKey: 'BpNG6BkFrQBQ3aOB3ao6yRMwGHnJzbFUt9k6MDZzNJ'
-	// });
+	const client = new MeiliSearch({
+		host: 'https://meilisearch-production-023b.up.railway.app/',
+		apiKey: 'BpNG6BkFrQBQ3aOB3ao6yRMwGHnJzbFUt9k6MDZzNJ'
+	});
 
-	// let hits = [];
-	// let processingTimeMs = 0;
-	// let estimatedTotalHits = 0;
+	let hits = [];
+	let processingTimeMs = 0;
+	let estimatedTotalHits = 0;
 
-	// const searchByQuery = async (event) => {
-	// 	const res = await client.index('topics').search(event?.target?.value);
+	const searchByQuery = async (event) => {
+		const res = await client.index('topics').search(event?.target?.value);
 
-	// 	console.log('res', res);
-	// 	hits = res?.hits;
-	// 	processingTimeMs = res?.processingTimeMs || 0;
-	// 	estimatedTotalHits = res?.estimatedTotalHits || 0;
-	// };
+		console.log('res', res);
+		hits = res?.hits;
+		processingTimeMs = res?.processingTimeMs || 0;
+		estimatedTotalHits = res?.estimatedTotalHits || 0;
+	};
 
-	// const debouncedSearchByQuery = debounce(searchByQuery, 200);
+	const debouncedSearchByQuery = debounce(searchByQuery, 200);
 </script>
 
 <PageContainer title="Home" layout="aside-main">
 	<Aside>aside</Aside>
 	<Main>
 		test
-		<!-- <div class="space-y-2">
+		<div class="space-y-2">
 			<TextInput on:input={debouncedSearchByQuery} />
 			<p class="text-sm text-base-content/80">
 				{hits?.length} of 7890 found in {processingTimeMs} milliseconds
 			</p>
 			<div class="space-y-2">
 				{#each hits as hit (hit.id)}
-					<TopicListItem topic={hit} />
+					<div>{hit?.name}</div>
+					<!-- <TopicListItem topic={hit} /> -->
 				{/each}
 			</div>
-		</div> -->
+		</div>
 	</Main>
 </PageContainer>
