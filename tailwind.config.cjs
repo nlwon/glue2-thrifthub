b0e18798040dadd7aea7bdd26f3da9ed18a8e3d9
Jay Joo <jj534@cornell.edu>
2023-01-10 17:50:15 +0900

    add prose plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,9 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
-	plugins: [require('daisyui'), require('@tailwindcss/line-clamp')]
+	plugins: [
+		require('daisyui'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography')
+	]
 };
