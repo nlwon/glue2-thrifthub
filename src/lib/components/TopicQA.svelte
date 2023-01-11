b4845e72140bc3905c881c2e9bef138188f81ad0
Jay Joo <jj534@cornell.edu>
2023-01-11 10:13:12 +0900

    improve topic details page performance

--- src/lib/components/TopicQA.svelte
+++ src/lib/components/TopicQA.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import axios from 'axios';
 	import { pb } from '$lib/glue/pocketbase';
-	import { format } from 'date-fns';
+	import { differenceInDays, format, formatDistance } from 'date-fns';
 	import IconUpArrow from '$lib/icons/glue/IconUpArrow.svelte';
 	import showdown from 'showdown';
 
@@ -10,14 +10,14 @@
 
 	let questionThreads = [];
 
-	const fetchQA = async () => {
+	const scrapeQAFromReddit = async () => {
 		const { data } = await axios(
 			`https://www.reddit.com/r/Cornell/search/.json?q=${encodeURIComponent(
 				topic?.name
 			)}&limit=30&restrict_sr=1&sr_nsfw=`
 		);
 
-		data?.data?.children?.map(async ({ data }) => {
+		const promises = data?.data?.children?.map(async ({ data }) => {
 			const { selftext, title, permalink } = data;
 
 			if (
@@ -27,17 +27,23 @@
 				const { data: postData } = await axios.get(`https://www.reddit.com${permalink}.json`);
 
 				const questionRawData = postData[0]?.data?.children[0]?.data;
-				const { id, selftext, created_utc } = questionRawData;
+				const { id, selftext, created_utc, media_metadata } = questionRawData;
+
+				if (media_metadata || title?.includes('[deleted]') || selftext?.includes('[deleted]'))
+					return null;
+
+				// NOTE: because a question can be for multiple topics
+				const providerId = `${topic?.id}-${id}`;
 
 				const questionData = {
 					title,
 					content: selftext,
 					provider: 'reddit',
-					providerId: id, //TODO: topicId-providerId
+					providerId,
 					providerData: questionRawData,
 					providerUrl: `https://www.reddit.com${permalink}`,
 					topic: topic?.id,
-					created: new Date(created_utc * 1000)
+					providerCreated: new Date(created_utc * 1000)
 				};
 
 				let question;
@@ -45,42 +51,47 @@
 				try {
 					question = await pb
 						.collection('questions')
-						.getFirstListItem(`providerId='${id}'`, { $autoCancel: false });
+						.getFirstListItem(`providerId='${providerId}'`, { $autoCancel: false });
+					question = await pb
+						.collection('questions')
+						.update(question?.id, questionData, { $autoCancel: false });
 				} catch (error) {
 					question = await pb.collection('questions').create(questionData, { $autoCancel: false });
 				}
 				question.answers = [];
 
 				if (postData?.length >= 2) {
-					// upsert comments
 					const comments = postData[1]?.data?.children;
 
 					for (let i = 0; i < comments?.length; i++) {
 						const commentDataRaw = comments[i]?.data;
-						const { body, id, permalink, score } = commentDataRaw;
-						console.log(question?.id, score, body);
+						const { body, id, permalink, score, created_utc } = commentDataRaw;
+						const providerId = `${topic?.id}-${id}`;
+
+						if (body?.includes('[deleted]')) return null;
 
 						if (score >= 1) {
 							const answerData = {
 								content: body,
 								provider: 'reddit',
-								providerId: id, //TODO: topicId-providerId
+								providerId,
 								providerData: commentDataRaw,
 								providerUrl: `https://www.reddit.com${permalink}`,
 								helpfulCount: score,
 								topic: topic?.id,
-								question: question?.id
-								// TODO: update created, updated
+								question: question?.id,
+								providerCreated: new Date(created_utc * 1000)
 							};
-
-							console.log(question?.id, answerData);
 
 							let answer;
 
 							try {
 								answer = await pb
 									.collection('answers')
-									.getFirstListItem(`providerId='${id}'`, { $autoCancel: false });
+									.getFirstListItem(`providerId='${providerId}'`, { $autoCancel: false });
+								answer = await pb
+									.collection('answers')
+									.update(answer?.id, answerData, { $autoCancel: false });
 							} catch (error) {
 								answer = await pb.collection('answers').create(answerData, { $autoCancel: false });
 							}
@@ -88,28 +99,69 @@
 						}
 					}
 				}
-				console.log('question', question);
-				questionThreads = [...questionThreads, question];
+
+				return question;
 			}
 		});
+
+		const scrapedQuestionThreads = await Promise.all(promises);
+		const existingQuestionIds = questionThreads?.map((question) => question?.id);
+		const newQuestionThreads = scrapedQuestionThreads?.filter(
+			(question) => question && !existingQuestionIds?.includes(question?.id)
+		);
+		questionThreads = [...questionThreads, ...newQuestionThreads];
 	};
 
-	onMount(fetchQA);
-	console.log('questionThreads', questionThreads);
+	const fetchQAFromDB = async () => {
+		const questions = await pb.collection('questions').getFullList(200, {
+			filter: `topic='${topic?.id}'`,
+			sort: '-created'
+		});
+		const promises = questions?.map(async (question) => {
+			const answers = await pb.collection('answers').getFullList(200, {
+				filter: `question='${question?.id}'`,
+				$autoCancel: false
+			});
+			return {
+				...question,
+				answers
+			};
+		});
+		questionThreads = await Promise.all(promises);
+	};
+
+	onMount(() => {
+		fetchQAFromDB();
+		scrapeQAFromReddit();
+	});
+	$: console.log('questionThreads', questionThreads);
 
 	const converter = new showdown.Converter();
 	converter?.setOption('openLinksInNewWindow', true);
+
+	const formatDate = (obj) => {
+		const date = obj?.providerCreated || obj?.created;
+		if (!date) return '';
+
+		if (differenceInDays(new Date(), new Date(date)) > 7) {
+			return format(new Date(date), 'yyyy-MM-dd hh:mm aaa');
+		} else {
+			return `${formatDistance(new Date(), new Date(date))} ago`;
+		}
+	};
 </script>
 
 <div class="">
-	<p class="text-3xl font-semibold">Q&A ({questionThreads?.length})</p>
+	<p class="text-3xl font-semibold">Discussions ({questionThreads?.length})</p>
 	<div class="">
 		{#each questionThreads as question (question?.id)}
 			<div class="space-y-4 border-b border-base-300 py-8">
 				<p class="text-xl font-semibold">Q. {question?.title}</p>
-				<p>{question?.content}</p>
+				<article class="prose prose-a:text-blue-600">
+					{@html converter.makeHtml(question?.content)}
+				</article>
 				<p class="mt-0.5 text-sm text-base-content/80">
-					{format(new Date(question?.created), 'yyyy-MM-dd hh:mm aaa')}
+					{formatDate(question)}
 				</p>
 				{#if question?.answers?.length > 0}
 					<div class="space-y-6 pt-2">
@@ -123,7 +175,7 @@
 										><IconUpArrow /> {answer?.helpfulCount} Helpful</button
 									>
 									<p class="mt-0.5 text-sm text-base-content/80">
-										{format(new Date(answer?.created), 'yyyy-MM-dd hh:mm aaa')}
+										{formatDate(answer)}
 									</p>
 								</div>
 							</div>
