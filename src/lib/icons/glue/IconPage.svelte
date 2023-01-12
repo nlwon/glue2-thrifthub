8305888c744ca077095cbe5c15394b15920cf1c4
Jay Joo <jj534@cornell.edu>
2023-01-12 11:49:47 +0900

    add icons

---
+++ src/lib/icons/glue/IconPage.svelte
@@ -0,0 +1,12 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+	><g
+		fill="none"
+		stroke="currentColor"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		strokeWidth="1.5"
+		><path
+			d="M4 21.4V2.6a.6.6 0 0 1 .6-.6h11.652a.6.6 0 0 1 .424.176l3.148 3.148A.6.6 0 0 1 20 5.75V21.4a.6.6 0 0 1-.6.6H4.6a.6.6 0 0 1-.6-.6ZM8 10h8m-8 8h8m-8-4h4"
+		/><path d="M16 2v3.4a.6.6 0 0 0 .6.6H20" /></g
+	></svg
+>
