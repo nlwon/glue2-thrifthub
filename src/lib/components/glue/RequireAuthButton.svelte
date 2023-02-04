fc1ea6dab64fd4b5dd269809435f86277cf64d21
Jay Joo <jj534@cornell.edu>
2023-02-04 14:13:52 -0500

    abstract out auth protected button to RequireAuthButton

---
+++ src/lib/components/glue/RequireAuthButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { currentUser } from '$lib/glue/pocketbase';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLButtonAttributes {
+		href?: string;
+	}
+
+	export let href: string = '';
+</script>
+
+{#if $currentUser}
+	{#if href}
+		<a {href}>
+			<button {...$$restProps} class={$$props.class}>
+				<slot />
+			</button>
+		</a>
+	{:else}
+		<button {...$$restProps} class={$$props.class}>
+			<slot />
+		</button>
+	{/if}
+{:else}
+	<label for="modal-auth" class={$$props.class}>
+		<slot />
+	</label>
+{/if}
