6ac190c29f7d54575c25fd289cbca3743e60e61f
Jay Joo <jj534@cornell.edu>
2023-02-08 06:57:08 -0500

    add feedback button hide routes

--- src/lib/components/glue/FeedbackModal.svelte
+++ src/lib/components/glue/FeedbackModal.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from '$app/stores';
 	import { APP_NAME } from '$lib/glue/config';
 	import { pb } from '$lib/glue/pocketbase';
 	import IconHappyFace from '$lib/icons/glue/IconHappyFace.svelte';
@@ -11,6 +12,7 @@
 	let step = 1;
 	let feedbackText = '';
 	let feedbackId = '';
+	const hideRouteIds = ['/chatrooms/[chatroomId]'];
 
 	const handleIconClick = async (review) => {
 		step = 2;
@@ -35,56 +37,58 @@
 	};
 </script>
 
-<div class="fixed bottom-4 right-4 md:right-1/4 md:bottom-6 md:-translate-y-1/2">
-	<label
-		for="modal-feedback"
-		class="btn-primary btn"
-		on:click={() => {
-			isOpen = true;
-			step = 1;
-		}}>Thoughts on {APP_NAME}?</label
-	>
-</div>
-<input type="checkbox" id="modal-feedback" class="modal-toggle" />
-<label for="modal-feedback" class={`modal cursor-pointer ${isOpen ? 'modal-open' : ''}`}>
-	<label class="modal-box relative box-border max-h-[70vh]" for="">
-		{#if step === 1}
-			<div class="prose">
-				<h2 class="font-bold">How was your experience with {APP_NAME}?</h2>
-				<div class="modal-action flex items-center justify-center space-x-1 md:space-x-6">
-					<button
-						class="rounded-xl p-4 text-6xl hover:bg-base-300"
-						on:click={() => handleIconClick('bad')}
-					>
-						<IconSadFace />
-					</button>
-					<button
-						class="rounded-xl p-4 text-6xl hover:bg-base-300"
-						on:click={() => handleIconClick('mid')}
-					>
-						<IconNeutralFace />
-					</button>
-					<button
-						class="rounded-xl p-4 text-6xl hover:bg-base-300"
-						on:click={() => handleIconClick('good')}
-					>
-						<IconHappyFace />
-					</button>
+{#if !hideRouteIds?.includes($page.route?.id)}
+	<div class="fixed bottom-4 right-4 md:right-1/4 md:bottom-6 md:-translate-y-1/2">
+		<label
+			for="modal-feedback"
+			class="btn-primary btn"
+			on:click={() => {
+				isOpen = true;
+				step = 1;
+			}}>Thoughts on {APP_NAME}?</label
+		>
+	</div>
+	<input type="checkbox" id="modal-feedback" class="modal-toggle" />
+	<label for="modal-feedback" class={`modal cursor-pointer ${isOpen ? 'modal-open' : ''}`}>
+		<label class="modal-box relative box-border max-h-[70vh]" for="">
+			{#if step === 1}
+				<div class="prose">
+					<h2 class="font-bold">How was your experience with {APP_NAME}?</h2>
+					<div class="modal-action flex items-center justify-center space-x-1 md:space-x-6">
+						<button
+							class="rounded-xl p-4 text-6xl hover:bg-base-300"
+							on:click={() => handleIconClick('bad')}
+						>
+							<IconSadFace />
+						</button>
+						<button
+							class="rounded-xl p-4 text-6xl hover:bg-base-300"
+							on:click={() => handleIconClick('mid')}
+						>
+							<IconNeutralFace />
+						</button>
+						<button
+							class="rounded-xl p-4 text-6xl hover:bg-base-300"
+							on:click={() => handleIconClick('good')}
+						>
+							<IconHappyFace />
+						</button>
+					</div>
 				</div>
-			</div>
-		{:else if step === 2}
-			<div class="prose">
-				<div class="flex items-start justify-between">
-					<h2 class="m-0 font-bold">Why did you feel that way?</h2>
-					<button on:click={handleCloseModal} class="btn-sm btn-circle btn">✕</button>
+			{:else if step === 2}
+				<div class="prose">
+					<div class="flex items-start justify-between">
+						<h2 class="m-0 font-bold">Why did you feel that way?</h2>
+						<button on:click={handleCloseModal} class="btn-sm btn-circle btn">✕</button>
+					</div>
+					<Textarea class="h-40" bind:value={feedbackText} />
+					<div class="mt-4 flex justify-center">
+						<button class="btn-primary btn-block btn" on:click={handleSubmitFeedbackText}
+							>Submit</button
+						>
+					</div>
 				</div>
-				<Textarea class="h-40" bind:value={feedbackText} />
-				<div class="mt-4 flex justify-center">
-					<button class="btn-primary btn-block btn" on:click={handleSubmitFeedbackText}
-						>Submit</button
-					>
-				</div>
-			</div>
-		{/if}
+			{/if}
+		</label>
 	</label>
-</label>
+{/if}
