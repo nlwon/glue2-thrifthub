d47a6aa587a9b753af8f09164d46ae12c1a3e612
Jay Joo <jj534@cornell.edu>
2023-02-09 16:51:34 -0500

    add chat and datestamp conditional rendering

--- src/lib/components/glue/chat/Chatroom.svelte
+++ src/lib/components/glue/chat/Chatroom.svelte
@@ -7,15 +7,47 @@
 
 	export let chatroom;
 
-	let chats = [];
+	let chatGroups = [];
 	let content = '';
 
 	const fetchChats = async () => {
-		console.log('chatroom?.id', chatroom?.id);
-		chats = await pb.collection('chats').getFullList(200, {
+		const chatDocs = await pb.collection('chats').getFullList(200, {
 			filter: `chatroom="${chatroom?.id}"`,
 			sort: 'created',
 			expand: 'sender,receiver'
+		});
+		console.log('chatDocs', chatDocs);
+
+		let prevChatUser = null;
+		let prevDateString = null;
+		chatGroups = [];
+
+		chatDocs?.forEach((chat) => {
+			const chatDateString = format(new Date(chat?.created), 'MMM dd');
+
+			if (!prevChatUser || prevDateString !== chatDateString) {
+				// first chat group || new date, new chat group
+				chatGroups?.push({
+					id: chatDateString,
+					variant: 'datestamp',
+					content: chatDateString
+				});
+				chatGroups?.push({
+					...chat,
+					items: [chat]
+				});
+			} else if (prevChatUser !== chat?.sender) {
+				// non-consecutive sender, new chat group
+				chatGroups?.push({
+					...chat,
+					items: [chat]
+				});
+			} else {
+				chatGroups[chatGroups?.length - 1]?.items?.push(chat);
+			}
+
+			prevChatUser = chat?.sender;
+			prevDateString = chatDateString;
 		});
 	};
 
@@ -36,23 +68,26 @@
 		content = '';
 	};
 	$: console.log('chatroom', chatroom);
-	$: console.log('chats', chats);
+	$: console.log('chatGroups', chatGroups);
 </script>
 
 {#if chatroom}
-	{#each chats as chat (chat?.id)}
-		<div class={`chat ${chat?.sender === $currentUser?.id ? 'chat-end' : 'chat-start'}`}>
-			<div class="chat-image avatar">
-				<div class="w-10 rounded-full">
-					<img src={chat?.expand?.sender?.avatarUrl} />
+	{#each chatGroups as group (group?.id)}
+		{#if group?.variant === 'datestamp'}
+			<div class="divider mt-0 mb-2 text-sm">{group?.content}</div>
+		{:else}
+			<div class={`chat ${group?.sender === $currentUser?.id ? 'chat-end' : 'chat-start'}`}>
+				<div class="chat-header mb-2">
+					{group?.expand?.sender?.name}
+					<time class="ml-1 text-xs opacity-50"
+						>{format(new Date(group?.created), 'hh:mm aaa')}</time
+					>
 				</div>
+				{#each group?.items as item (item?.id)}
+					<div class="chat-bubble mb-2">{item?.content}</div>
+				{/each}
 			</div>
-			<div class="chat-header mb-2">
-				{chat?.expand?.sender?.name}
-				<time class="ml-1 text-xs opacity-50">{format(new Date(chat?.created), 'hh:mm aaa')}</time>
-			</div>
-			<div class="chat-bubble">{chat?.content}</div>
-		</div>
+		{/if}
 	{/each}
 	<div class="fixed bottom-0 left-0 right-0 bg-base-300 p-4">
 		<form on:submit={handleCreateChat} class="flex items-center space-x-2">
