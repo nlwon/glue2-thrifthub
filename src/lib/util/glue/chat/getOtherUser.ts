261f1dd8b42714edc6fb07c4310a7984a3b87b24
Jay Joo <jj534@cornell.edu>
2023-02-15 13:59:24 -0500

    add is expanded option

--- src/lib/util/glue/chat/getOtherUser.ts
+++ src/lib/util/glue/chat/getOtherUser.ts
@@ -1,6 +1,11 @@
-const getOtherUser = ({ chatroom, user }) => {
-	if (chatroom?.author === user?.id) return chatroom?.searcher;
-	return chatroom?.author;
+const getOtherUser = ({ chatroom, user, isExpanded = false }) => {
+	if (isExpanded) {
+		if (chatroom?.author === user?.id) return chatroom?.expand?.searcher;
+		return chatroom?.expand?.author;
+	} else {
+		if (chatroom?.author === user?.id) return chatroom?.searcher;
+		return chatroom?.author;
+	}
 };
 
 export default getOtherUser;
