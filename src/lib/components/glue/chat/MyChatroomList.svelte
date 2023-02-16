26619782e6b969e09067f03a94b5c730a94bb876
Jay Joo <jj534@cornell.edu>
2023-02-16 14:53:11 -0500

    enlarge text on mobile

--- src/lib/components/glue/chat/MyChatroomList.svelte
+++ src/lib/components/glue/chat/MyChatroomList.svelte
@@ -43,7 +43,7 @@
 <div class="h-[70vh] rounded border-base-content/20 md:h-[80vh] md:border">
 	<div class="space-y-2 pl-4 pb-2 md:p-4">
 		<p class="text-2xl font-semibold">Messages</p>
-		<p class="text-xs text-base-content/70">
+		<p class="text-sm text-base-content/70 md:text-xs">
 			You'll receive an email notification when someone messages you!
 		</p>
 	</div>
