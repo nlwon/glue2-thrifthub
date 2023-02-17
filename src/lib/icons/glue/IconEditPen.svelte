180b9e921f36b10eb114182a1a9cb120cb01b2b6
Jay Joo <jj534@cornell.edu>
2023-02-17 15:06:01 -0500

    add icons

---
+++ src/lib/icons/glue/IconEditPen.svelte
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+	><path
+		fill="currentColor"
+		d="m19.3 8.925l-4.25-4.2l1.4-1.4q.575-.575 1.413-.575q.837 0 1.412.575l1.4 1.4q.575.575.6 1.388q.025.812-.55 1.387ZM17.85 10.4L7.25 21H3v-4.25l10.6-10.6Z"
+	/></svg
+>
