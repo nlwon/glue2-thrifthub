9253466d3932a27af117cfb9ef75867d6bd9de23
Jay Joo <jj534@cornell.edu>
2023-02-18 10:41:22 -0500

    add filter list icon

---
+++ src/lib/icons/glue/IconFilterList.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 2.4 24 24"
+	><path fill="currentColor" d="M10 20.4v-2h4v2h-4Zm-4-5v-2h12v2H6Zm-3-5v-2h18v2H3Z" /></svg
+>
