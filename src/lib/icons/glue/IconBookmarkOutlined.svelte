74cddb112e99c06a8065b5dae06e2dc32ac78fe5
Jay Joo <jj534@cornell.edu>
2023-02-24 18:26:46 -0500

    add bookmark icons

---
+++ src/lib/icons/glue/IconBookmarkOutlined.svelte
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+	><path
+		fill="currentColor"
+		d="m7 17.95l5-2.15l5 2.15V5H7v12.95Zm-.6 2.45q-.5.2-.95-.088T5 19.476V5q0-.825.588-1.413T7 3h10q.825 0 1.413.588T19 5v14.475q0 .55-.45.838t-.95.087L12 18l-5.6 2.4ZM7 5h10H7Z"
+	/></svg
+>
