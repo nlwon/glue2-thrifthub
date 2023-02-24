776e2377b19a37c44f3b5ef0478c7f17cf3c74f0
Jay Joo <jj534@cornell.edu>
2023-02-24 17:00:31 -0500

    sticky header to top

--- src/lib/components/glue/AppLayout.svelte
+++ src/lib/components/glue/AppLayout.svelte
@@ -11,63 +11,66 @@
 		<!-- mobile nav: invisible drawer toggle -->
 		<input id="drawer-mobile-nav" type="checkbox" class="drawer-toggle" />
 		<div class="drawer-content flex flex-col items-center">
-			<div class={`w-full max-w-4xl`}>
-				<div class="navbar bg-base-100">
-					<!-- mobile nav: hamburger -->
-					{#if PUBLIC_NAVS?.length > 0}
-						<div class="flex-none md:hidden">
-							<label for="drawer-mobile-nav" class="btn-ghost drawer-button btn-square btn">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									class="inline-block h-5 w-5 stroke-current"
-									><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M4 6h16M4 12h16M4 18h16"
-									/></svg
-								>
-							</label>
+			<!-- header -->
+			<div class="sticky top-0 z-30 flex w-full justify-center bg-base-100/95">
+				<div class={`w-full max-w-4xl`}>
+					<div class="navbar">
+						<!-- mobile nav: hamburger -->
+						{#if PUBLIC_NAVS?.length > 0}
+							<div class="flex-none md:hidden">
+								<label for="drawer-mobile-nav" class="btn-ghost drawer-button btn-square btn">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										class="inline-block h-5 w-5 stroke-current"
+										><path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M4 6h16M4 12h16M4 18h16"
+										/></svg
+									>
+								</label>
+							</div>
+						{/if}
+
+						<!-- app name -->
+						<div>
+							<button>
+								<a href="/" class="btn-ghost btn text-xl normal-case"
+									>{APP_NAME}
+									{#if IS_BETA}
+										<span class="ml-1.5 text-base-content/60">beta</span>
+									{/if}
+								</a>
+							</button>
 						</div>
-					{/if}
 
-					<!-- app name -->
-					<div>
-						<button>
-							<a href="/" class="btn-ghost btn text-xl normal-case"
-								>{APP_NAME}
-								{#if IS_BETA}
-									<span class="ml-1.5 text-base-content/60">beta</span>
-								{/if}
-							</a>
-						</button>
-					</div>
-
-					<!-- right side menu -->
-					<div class="flex flex-1 justify-center">
-						<div class="hidden md:block">
-							<div class="menu menu-horizontal p-2">
-								{#if PUBLIC_NAVS?.length > 0}
-									{#each PUBLIC_NAVS as nav}
-										<li class="font-bold"><a href={nav.path}>{nav.label}</a></li>
-									{/each}
-								{/if}
+						<!-- right side menu -->
+						<div class="flex flex-1 justify-center">
+							<div class="hidden md:block">
+								<div class="menu menu-horizontal p-2">
+									{#if PUBLIC_NAVS?.length > 0}
+										{#each PUBLIC_NAVS as nav}
+											<li class="font-bold"><a href={nav.path}>{nav.label}</a></li>
+										{/each}
+									{/if}
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="flex-none">
-						<div class="hidden md:block">
-							<div class="menu menu-horizontal p-2">
-								{#if PUBLIC_NAVS?.length > 0}
-									{#each PUBLIC_NAVS as nav}
-										<li class="font-medium"><a href={nav.path}>{nav.label}</a></li>
-									{/each}
-								{/if}
+						<div class="flex-none">
+							<div class="hidden md:block">
+								<div class="menu menu-horizontal p-2">
+									{#if PUBLIC_NAVS?.length > 0}
+										{#each PUBLIC_NAVS as nav}
+											<li class="font-medium"><a href={nav.path}>{nav.label}</a></li>
+										{/each}
+									{/if}
+								</div>
 							</div>
+							<Auth />
 						</div>
-						<Auth />
 					</div>
 				</div>
 			</div>
