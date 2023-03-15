8f83e3a3e983c6d0de6103c472830c67ce47402a
Jay Joo <jj534@cornell.edu>
2023-03-15 18:59:21 -0400

    add private routes

--- src/lib/glue/config.ts
+++ src/lib/glue/config.ts
@@ -1,9 +1,21 @@
+import IconBookmarkOutlined from '$lib/icons/glue/IconBookmarkOutlined.svelte';
+import IconMenu from '$lib/icons/glue/IconMenu.svelte';
 import IconMessage from '$lib/icons/glue/IconMessage.svelte';
 import type { IPublicNav, IPrivateNav } from '$lib/types/glue/nav.type';
 
 export const APP_NAME = 'ThriftHub';
 export const PUBLIC_NAVS: IPublicNav[] = [];
 export const PRIVATE_NAVS: IPrivateNav[] = [
+	{
+		label: 'My listings',
+		path: '/my-listings',
+		icon: IconMenu
+	},
+	{
+		label: 'Bookmarks',
+		path: '/bookmarks',
+		icon: IconBookmarkOutlined
+	},
 	{
 		label: 'Messages',
 		path: '/chatrooms',
