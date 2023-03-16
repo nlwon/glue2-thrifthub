b8ec4f61bd1d81e0624357e87ce6530745ea89a3
Jay Joo <jj534@cornell.edu>
2023-03-16 03:47:00 -0400

    implement my listings page

--- src/routes/my-listings/+page.svelte
+++ src/routes/my-listings/+page.svelte
@@ -1,1 +1,29 @@
-my listings
+<script lang="ts">
+	import PageContainer from '$lib/components/glue/PageContainer.svelte';
+	import ListingItem from '$lib/components/ListingItem.svelte';
+	import { currentUser, pb } from '$lib/glue/pocketbase';
+	import { onMount } from 'svelte';
+
+	let listings = [];
+
+	const fetchListings = async () => {
+		try {
+			listings = await pb.collection('listings').getFullList(200, {
+				filter: `user="${$currentUser?.id}"`,
+				expand: 'user'
+			});
+		} catch (error) {}
+	};
+
+	onMount(() => {
+		fetchListings();
+	});
+</script>
+
+<PageContainer title="My listings">
+	<div class="space-y-2">
+		{#each listings as listing (listing?.id)}
+			<ListingItem {listing} />
+		{/each}
+	</div>
+</PageContainer>
