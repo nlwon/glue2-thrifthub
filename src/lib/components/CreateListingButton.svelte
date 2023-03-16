ab1f12032cb1c6441eaa27f539be8a1eb0c817b1
Jay Joo <jj534@cornell.edu>
2023-03-16 03:46:33 -0400

    shorten create listing button label in mobile

--- src/lib/components/CreateListingButton.svelte
+++ src/lib/components/CreateListingButton.svelte
@@ -1,8 +1,11 @@
 <script>
 	import IconAdd from '$lib/icons/glue/IconAdd.svelte';
+	import { isMd } from '$lib/stores/glue/device';
 	import RequireAuthButton from './glue/RequireAuthButton.svelte';
+
+	$: label = $isMd ? 'New listing' : 'Listing';
 </script>
 
 <RequireAuthButton href="/create" class="btn-primary btn gap-2 pl-3">
-	<span class="text-lg"><IconAdd /></span>New listing
+	<span class="text-lg"><IconAdd /></span>{label}
 </RequireAuthButton>
