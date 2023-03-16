d2261d4a5f122d8654edfb1b079873eb86702e64
armita-jamshidi <aaj34@cornell.edu>
2023-03-16 18:08:56 -0400

    Adding filters test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,13 @@
 <PageContainer title="Home" layout="aside-main">
 	
 	<Aside>
+		<div id="myBtnContainer">
+			<button class="btn active" onclick="filterSelection('all')"> Show all</button>
+			<button class="btn" onclick="filterSelection('cars')"> Cars</button>
+			<button class="btn" onclick="filterSelection('animals')"> Animals</button>
+			<button class="btn" onclick="filterSelection('fruits')"> Fruits</button>
+			<button class="btn" onclick="filterSelection('colors')"> Colors</button>
+		</div>
 		<input type="checkbox" checked="checked" class="checkbox" />
 		<label for="my-modal" class="btn">open modal</label>
 	</Aside>
