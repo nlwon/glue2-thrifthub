cd0790b6b2acac216a3c59e993a29e2f88b159c8
Jay Joo <jj534@cornell.edu>
2023-03-17 16:51:31 -0400

    fix listing modal layout and loading state button bugs

--- src/lib/components/ListingModal.svelte
+++ src/lib/components/ListingModal.svelte
@@ -36,19 +36,24 @@
 	<input type="checkbox" id="modal-listing-{listing?.id}" class="modal-toggle" />
 	<label for="modal-listing-{listing?.id}" class="modal cursor-pointer">
 		<label class="modal-box relative w-[80vw] max-w-none md:w-[60vw]" for="">
-			<div class="flex justify-center">
-				<div class=" space-y-4 ">
-					<div class="carousel-center carousel rounded-box h-[40vh] space-x-4 bg-neutral p-4">
-						{#each listing?.photos as photo}
-							<div class="carousel-item">
-								<img
-									src={pb.getFileUrl(listing, photo)}
-									class="rounded-box w-[50vw] object-cover md:w-[30vw]"
-									alt=""
-								/>
-							</div>
-						{/each}
+			<div class="flex w-full justify-center">
+				<div class=" w-full space-y-4">
+					<!-- carousel -->
+					<div>
+						<div class="carousel-center carousel rounded-box h-[40vh] space-x-4 bg-neutral p-4">
+							{#each listing?.photos as photo}
+								<div class="carousel-item">
+									<img
+										src={pb.getFileUrl(listing, photo)}
+										class="rounded-box w-[50vw] object-cover md:w-[30vw]"
+										alt=""
+									/>
+								</div>
+							{/each}
+						</div>
 					</div>
+
+					<!-- info -->
 					<div class="space-y-1">
 						<h3 class="text-2xl font-semibold">{listing?.title}</h3>
 						<p class="text-lg font-semibold">${listing?.price}</p>
@@ -61,16 +66,24 @@
 									{listing?.desc}
 								</p>
 							</div>
-							<button
-								class="btn-primary btn hidden md:block {isChatLoading && 'loading'}"
-								on:click={handleChatClick}>Message seller</button
-							>
+
+							<!-- message button (desktop) -->
+							<div class="hidden md:block">
+								<button
+									class="btn-primary btn {isChatLoading && 'loading'}"
+									on:click={handleChatClick}>Message seller</button
+								>
+							</div>
 						</div>
 					</div>
-					<button
-						class="btn-primary btn-block btn mt-4 md:hidden {isChatLoading && 'loading'}"
-						on:click={handleChatClick}>Message seller</button
-					>
+
+					<!-- message button (mobile) -->
+					<div class="md:hidden">
+						<button
+							class="btn-primary btn-block btn mt-4 {isChatLoading && 'loading'}"
+							on:click={handleChatClick}>Message seller</button
+						>
+					</div>
 				</div>
 			</div>
 		</label>
