d5a92cb289733c320b24ea3a261f488aad305582
Nicholas Won <nlw55@cornell.edu>
2023-04-23 02:36:31 -0400

    adjusted welcome text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,11 +26,11 @@
 <PageContainer title="Home" layout="aside-main">
 	<Aside>
 		<div class="hidden space-y-3 rounded-lg bg-base-200 p-4 fixed md:block w-[290px]">
-			<p class="font-semibold text-base-content/90">Welcome to ThriftHub! &#128717;&#65039;</p>
+			<p class="font-semibold text-base-content/90">Welcome to ThriftHub! &#129297;</p>
 			<div class="space-y-1 text-base-content/70">
 				<p class="text-sm text-divider">The localized marketplace for Cornell, designed for clothing and item resale!</p>
 				<p class="text-sm">Share suggestions and feedback to: 
-					<a target="_blank" href="mailto:thrifthub.webdev@gmail.com" class="transition-colors hover:text-gray-400">
+					<a target="_blank" href="mailto:thrifthub.webdev@gmail.com" class="unselectable transition-colors hover:text-gray-400">
 						<svg style="margin-bottom: 2px; display: inline;" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
 							<path fill="currentColor" d="M4 20q-.825 0-1.412-.587Q2 18.825 2 18V6q0-.825.588-1.412Q3.175 4 4 4h16q.825 0 1.413.588Q22 5.175 
 							22 6v12q0 .825-.587 1.413Q20.825 20 20 20ZM20 8l-7.475 4.675q-.125.075-.263.112q-.137.038-.262.038t-.262-.038q-.138-.037-.263-.112L4 
@@ -75,6 +75,10 @@
 
 <!-- custom styles -->
 <style>
+	.unselectable {
+		user-select: none;
+	}
+	
 	.text-divider {
 		border-bottom-width: 1px;
 		padding-bottom: 10px;
